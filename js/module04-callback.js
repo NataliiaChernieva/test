a7c9acdb80514a8dae371667b12679939bf4d5ef
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-03-10 00:01:41 +0200

    добавляет задачи в 4 модуль

---
+++ js/module04-callback.js
@@ -0,0 +1,235 @@
+// const filter = function (array, test) {
+//   const filteredElements = [];
+
+//   for (const element of array) {
+//     const passed = test(element);
+
+//     if (passed) {
+//       filteredElements.push(element);
+//     }
+//   }
+
+//   return filteredElements;
+// };
+
+// const fruits = [
+//   { name: 'apples', quantity: 200, isFresh: true },
+//   { name: 'grapes', quantity: 150, isFresh: false },
+//   { name: 'bananas', quantity: 100, isFresh: true },
+// ];
+
+// const freshFruits = filter(fruits, fruit => fruit.isFresh);
+// console.log(freshFruits); // массив с объектами apples и bananas
+
+// const fruitsWithQuantity = filter(fruits, fruit => fruit.quantity >= 120);
+// console.log(fruitsWithAmount); // массив с объектами apples и grapes
+
+
+//Задача на замыкание. Напишем функцию приготовления поваром некого блюда.
+
+// const makeShef = function (name) {
+//   /*
+//    * Параметр name это локальная переменная для функции makeShef.
+//    * Это значит что она будет доступна функции makeDish через замыкание.
+//    */
+//   return function makeDish(dish) {
+//     console.log(`${name} is cooking ${dish}`);
+//   };
+// };
+
+// const mango = makeShef('Mango');
+// mango('apple pie'); // Mango is cooking apple pie
+// mango('beef stew'); // Mango is cooking beef stew
+
+// const poly = makeShef('Poly');
+// poly('pancakes'); // Poly is cooking pancakes
+// poly('eggs and bacon'); // Poly is cooking eggs and bacon
+
+/*
+ * Фильтр
+ */
+
+// const filter = (array, test) => {
+//     const filteredArray = [];
+
+//     for (const el of array) {
+//         console.log(el);
+//         const passed = test(el);
+
+//         if (passed) {
+//             filteredArray.push(el);
+//         }
+//     }
+
+//     return filteredArray;
+// };
+
+// // 1. надо передать функцию
+// // 2. функция получает элемент массива
+// // 3. если элемент массива удовлетворяет условию то функция вернет true
+// // 3. если элемент массива НЕ удовлетворяет условию то функция вернет false
+
+// const callback1 = value => {
+//     return value >= 3;
+// };
+
+// const r1 = filter([1, 2, 3, 4, 5], callback1);
+// console.log(r1);
+
+// const callback2 = value => {
+//     return value <= 4;
+// };
+
+// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callback2);
+// console.log(r2);
+
+// const fruits = [
+//     { name: 'apples', quantity: 200, isFresh: true },
+//     { name: 'grapes', quantity: 150, isFresh: false },
+//     { name: 'bananas', quantity: 100, isFresh: true },
+// ];
+
+// const getFruitsWithQuantity = fruit => {
+//     return fruit.quantity >= 120;
+// };
+
+// const r3 = filter(fruits, getFruitsWithQuantity);
+// console.log(r3);
+
+/*
+ * Округлятор 🤷‍♂️
+ */
+
+// const floatingPoint = 3.456789;
+// const someInt = Math.round(floatingPoint); // 3
+// const withDecimals = Number(floatingPoint.toFixed(2)); // 3.46
+
+// const rounder = function (number, places) {
+//     return Number(number.toFixed(places));
+// };
+
+// console.log(rounder(3.4567, 2));
+// console.log(rounder(3.4567, 3));
+// console.log(rounder(5.1234, 2));
+// console.log(rounder(3.4567, 3));
+
+// const rounder = function (places) {
+//     return function (number) {
+//         return Number(number.toFixed(places));
+//     };
+// };
+
+// const rounder2 = rounder(2);
+// const rounder3 = rounder(3);
+
+// console.dir(rounder2);
+// console.dir(rounder3);
+
+// console.log(rounder2(3.4567));
+// console.log(rounder2(5.4512312312367));
+// console.log(rounder3(3.4567));
+// console.log(rounder2(5.1234));
+// console.log(rounder3(3.4567));
+// console.log(rounder3(10.67667));
+
+
+///////Задание 4/10
+// const pizzaPalace = {
+//   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+//   order(pizzaName, onSuccess, onError) {
+//       for(let pizza of this.pizzas){
+//           if (pizzaName === pizza) {
+//               return onSuccess(pizzaName);
+//            }
+//       }
+//       return onError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
+//   },
+// };
+// // Пиши код выше этой строки
+
+// // Колбэк для onSuccess
+// function makePizza(pizzaName) {
+//   return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+// }
+
+// // Колбэк для onError
+// function onOrderError(error) {
+//   return `Ошибка! ${error}`;
+// }
+
+// // Вызовы метода с колбэками
+// pizzaPalace.order('Аль Копчино', makePizza, onOrderError);
+// pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
+// pizzaPalace.order('Биг майк', makePizza, onOrderError);
+// pizzaPalace.order('Венская', makePizza, onOrderError);
+
+
+/////////Задание 7/10
+// Сервису приготовления и доставки еды требуется функция генерации сообщений о статусе заказа.
+// Дополни функцию composeMessage(position) так, чтобы она возвращала строку 
+//в формате 'Готовим <блюдо> для <почта>. Ваш заказ <позиция>-й в очереди.' Позиция это 
+//значение параметра position - позиция элемента в массиве(на единицу больше чем индекс).
+// Не объявляй дополнительные параметры функции composeMessage(position).
+// Используй call для вызова функции в контексте одного объекта-заказа.
+// Используй this в теле функции для доступа к свойствам объекта-заказа в контексте которого она была вызывана.
+
+// const orders = [
+//   { email: 'solomon@topmail.ua', dish: 'Burger' },
+//   { email: 'artemis@coldmail.net', dish: 'Pizza' },
+//   { email: 'jacob@mail.com', dish: 'Taco' },
+// ];
+
+// // Пиши код ниже этой строки
+// function composeMessage(position) {
+//    return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+// }
+ 
+
+// 1 способ
+//  const messages = [];
+// for (let index = 0; index < orders.length; index++){
+//     const order = orders[index];
+//     const message = composeMessage.call(order, index + 1);
+//     messages.push(message);
+// }
+
+// //2способ
+// const messages = orders.map(function (order, index) {
+//     return composeMessage.call(order, index + 1);   
+// })
+
+// console.log(messages);
+
+
+////Задание 10\10
+const service = {
+  mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+  subscribe(email) {
+    this.mailingList.push(email);
+    return `Почта ${email} добавлена в рассылку.`;
+  },
+  unsubscribe(email) {
+    this.mailingList = this.mailingList.filter((e) => e !== email);
+    return `Почта ${email} удалена из рассылки.`;
+  },
+};
+
+function logAndInvokeAction(email, action) {
+  console.log(`Выполняем действие с ${email}.`);
+  return action(email);
+}
+
+const firstInvoke = logAndInvokeAction('kiwi@mail.uk', service.subscribe.bind(service));
+console.log(firstInvoke);
+// Почта kiwi@mail.uk добавлена в рассылку.
+
+console.log(service.mailingList);
+/* ['mango@mail.com', 
+    'poly@hotmail.de', 
+    'ajax@jmail.net', 
+    'kiwi@mail.uk']*/
+const secondInvoke = logAndInvokeAction('poly@hotmail.de', service.unsubscribe.bind(service));
+console.log(secondInvoke);
+// Почта poly@hotmail.de удалена из рассылки.
+
+console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
